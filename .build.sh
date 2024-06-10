587b976d704fd13c95f875ec7cb470ca7c5b2c26
noureddin <noureddin@protonmail.com>
2024-06-10 19:26:25 +0200

    replace Make with Bash
    Make wouldn't build a.gz regardless of whatever I do, and
    re-implementing what I needed of it in Bash was a ten-minute task.

---
+++ .build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+declare out
+declare -a dep
+
+wanted() {
+  if ! [ -e "$out" ]; then return 0; fi
+  for f in "${dep[@]}"; do
+    if [ "$f" -nt "$out" ]; then return 0; fi
+  done
+  return 1
+}
+
+if [ "$1" = "-B" ] || [ "$1" = force ] || [ "$1" = clean ]; then
+  echo cleaning
+  grep ^out= "${BASH_SOURCE[0]}" | sed s,^out=,, | while read line; do rm -f "$line"; done
+  if [ "$1" = clean ]; then exit 0; fi
+fi
+
+out=.w.txt
+dep=(.a.js .utils.js .writewords.js)
+if wanted; then
+  echo building $out
+  cat "${dep[@]}" | deno run - > $out
+fi
+
+out=.p.txt
+dep=(.a.js .utils.js .calcuniqprefix.js)
+if wanted; then
+  echo building $out
+  cat "${dep[@]}" | deno run - > $out
+fi
+
+out=a.gz
+dep=(.a.txt .w.txt .p.txt)
+if wanted; then
+  echo building $out
+  cat "${dep[@]}" | perl -ne '/[0-9]/ ? ($ans .= $_) : print; END { print $ans }' | gzip --best > $out
+  # that perl is to collect answers at the end for better compression (-22K)
+fi
+
+out=.g.js
+dep=(.g.ts .g.sh)
+if wanted; then
+  echo building $out
+  bash .g.sh
+fi
+
+out=index.html
+dep=(a.gz .g.js .g.js .utils.js .make.js .logic.js .style.css .index.html .process.pl)
+if wanted; then
+  echo building $out
+  perl -CDAS .process.pl .index.html > index.html
+fi
+
--- Makefile
+++ Makefile
@@ -1,15 +1,4 @@
-index.html: a.gz .g.js .*.js .style.css .index.html .process.pl
-	perl -CDAS .process.pl .index.html > index.html
+all:
+	@bash .build.sh
 
-.w.txt: .a.js .utils.js .writewords.js
-	cat $^ | deno run - > $@
-
-.p.txt: .a.js .utils.js .calcuniqprefix.js
-	cat $^ | deno run - > $@
-
-a.gz: .a.txt .w.txt .p.txt
-        cat $^ | perl -ne '/[0-9]/ ? ($$ans .= $$_) : print; END { print $$ans }' | gzip --best > $@
-# that perl is to collect answers at the end for better compression (-22K)
-
-.g.js: .g.ts .g.sh
-	bash .g.sh
+.PHONY: all
